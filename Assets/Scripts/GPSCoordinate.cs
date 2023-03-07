a8714a19dcd0de2ac63c9e12940081b2bad6019c
Ramatoulaye Balde <202157708@edu.clg.qc.ca>
2023-03-07 17:35:15 -0500

    Scene transfer

---
+++ Assets/Scripts/GPSCoordinate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSCoordinate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static void CalculerDistanceEntreDeuxCoordonnées(GPSCoordinate A, GPSCoordinate B)
+    {
+        
+    }
+}

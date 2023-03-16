13827e021e05d991c46b1009b127174bd70160ce
Ramatoulaye Balde <202157708@edu.clg.qc.ca>
2023-03-16 02:36:26 -0400

    Game AI change name

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameAI
+{
+    namespace Pathfinding
+    {
+        public enum PathFinderStatus
+        {
+            PAS_INITIALISÉ,
+            SUCCES,
+            ÉCHEC,
+            EN_MARCHE,
+        }
+    }
+}

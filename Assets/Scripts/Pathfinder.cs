27e1baf64388955b98bc37ed170253828664702b
Ramatoulaye Balde <202157708@edu.clg.qc.ca>
2023-03-18 13:51:51 -0400

    change to assets

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -1,17 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using GameAI.Pathfinding;
 using UnityEngine;
 
 namespace GameAI
 {
     namespace Pathfinding
     {
-        public enum PathFinderStatus
+        public enum StatutPathfinder
         {
             PAS_INITIALISÉ,
             SUCCES,
             ÉCHEC,
             EN_MARCHE,
         }
+        abstract public class PathFinder<T>
+        {
+            public Node Départ { get; private set; }
+            public Node Destination { get; private set; }
+            
+            //check what needs to b unprivated/privated
+        
+            public StatutPathfinder Statut
+            {
+                get;
+                set;
+            } = StatutPathfinder.PAS_INITIALISÉ;
+
+            public delegate float CalculerCoût(Node a, Node b);
+            public CalculerCoût CoûtHeuristique { get; set; }
+            //not a real word
+            public CalculerCoût CoûtTransnodal { get; set; }
+            
+            
+            //pt mettre node class abstraite pr que ca herite de
+            public class NodePathfinder
+            {
+                //la node jst avant quon arrive au nodepathfindr
+                public NodePathfinder Parent { get; set; }
+                
+                public Node Location{}
+                
+            }
+
+
+
+        }
+        
     }
+
+    
 }

4eea00d230cd3912eb5d9d0c1d95bec0e93a1978
Ramatoulaye Balde <202157708@edu.clg.qc.ca>
2023-03-23 06:13:47 -0400

    InstancierNodes method in Node class

--- Assets/Scripts/PathfindingNode.cs
+++ Assets/Scripts/PathfindingNode.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathfindingN : MonoBehaviour
+public class PathfindingNode : Node
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public bool EstEndroitPublic { get; set; } //anything that isnt a classsroom
+
+    public bool EstTraversable { get; set; }
+
+    public List<int> ConnectedNodes { get; set; }
+
+    public PathfindingNode ParentNode { get; set; }
+
+    public bool EstDansClosedList { get; set; } = false;
+
+    public bool EstDansOpenList { get; set; } = false;
+
+    public PathfindingNode Départ { get; set; }
+
+    public float FCost { get; set; }
+    public float HCost { get; set; }
+    public float GCost { get; set; }
+
+
+    // Start is called before the first frame update
+    public PathfindingNode(int nombre, string nom, bool estEndroitPublic, Étage étage,
+        List<int> connectedNodes, GPSCoordinate coordonéesGps) : base(nombre, nom, étage, coordonéesGps)
     {
-        
+        ConnectedNodes = connectedNodes;
+        EstEndroitPublic = estEndroitPublic;
+
+
     }
 }

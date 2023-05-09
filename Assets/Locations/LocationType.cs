c30c7e2785754376898cf61eaf49a22c7998c69c
Ramatoulaye Balde <202157708@edu.clg.qc.ca>
2023-05-09 16:07:46 -0400

    Prefabs.meta

---
+++ Assets/Locations/LocationType.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Pathfinding.Locations
+{
+    public abstract class LocationType<T>
+    {
+        public abstract string Name { get; set; }
+        public abstract List<T> Locations { get; set; }
+
+        public virtual void OnSelected()
+        {
+            
+        }
+
+        public virtual void OnUnselected()
+        {
+            
+        }
+    }
+}
